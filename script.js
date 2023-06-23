d5d367f02fabdfe559a3f3227bb9fc3a0f71da7a
Austine Gomez <austine.gomez@yahoo.com>
2023-06-22 20:48:08 -0500

    mobile css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('active');
+
+    if (button.classList.contains('active')) {
+      buttons.forEach((btn) => {
+        if (btn !== button) btn.classList.remove('active');
+      });
+    }
+  });
+});
